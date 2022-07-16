27b7f72b4523cafb3c5d2cb684eb8f313c022e40
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-16 07:28:24 +0000

    gitpod and test

--- src/main/kotlin/be/alfu32/kotlin/starter/Main.kt
+++ src/main/kotlin/be/alfu32/kotlin/starter/Main.kt
@@ -7,5 +7,9 @@ fun main(args: Array<String>){
     }
     val c = MyCalculator()
     val list = listOf(1,2,3,4,5)
+    val group = list.groupBy{
+        v -> if(v%2 == 0){"even"}else{"odd"}
+    }
+    println("grouped list ${group}")
     println("""MyCalculator.sum($list) = ${c.sum(list)}""")
 }
