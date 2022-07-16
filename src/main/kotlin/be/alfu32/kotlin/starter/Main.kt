d7f953cc0901d9cafcda0846622befe7866f7d26
ioan alferaru <ioan.alferaru@gmail.com>
2022-07-16 07:02:37 +0000

    initial commit

--- src/main/kotlin/be/alfu32/kotlin/starter/Main.kt
+++ src/main/kotlin/be/alfu32/kotlin/starter/Main.kt
@@ -2,8 +2,10 @@ package be.alfu32.kotlin.starter
 
 fun main(args: Array<String>){
     println("it works with args")
-    args.forEach {
-        arg -> println("arg - $arg")
+    args.forEachIndexed {
+        i,arg -> println("""arg[$i]='$arg'""")
     }
-    println(args)
+    val c = MyCalculator()
+    val list = listOf(1,2,3,4,5)
+    println("""MyCalculator.sum($list) = ${c.sum(list)}""")
 }
