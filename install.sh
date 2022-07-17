128d2f54535180e0f1d16bd9aa81267f27c239ff
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-17 05:22:37 +0000

    install script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 starter_name=starter-kotlin-maven
 repo=https://github.com/alfu32/$starter_name.git
+rand_tag=`echo $RANDOM | md5sum | head -c 20`
 
-git clone $repo
+echo $rand_tag
+exit
 
-cd $starter_name
+git clone $repo "$starter_name-$rand_tag"
+
+cd "$starter_name-$rand_tag"
 
 rm -rf .git
 
