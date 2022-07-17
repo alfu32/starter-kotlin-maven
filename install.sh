2a8b1eeeb23a109f0d5db11b48d43cbd64dd43eb
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-17 05:13:46 +0000

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-repo=https://github.com/alfu32/starter-kotlin-maven.git
-cd
+starter_name=starter-kotlin-maven
+repo=https://github.com/alfu32/$starter_name.git
+
 git clone $repo
+
+cd $starter_name
+
 rm -rf .git
+
 git init
+
