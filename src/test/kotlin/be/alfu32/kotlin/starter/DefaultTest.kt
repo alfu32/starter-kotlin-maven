e2e3cb635091fd6e28048efd7cc516b124012e32
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-25 16:51:23 +0200

    Update DefaultTest.kt

--- src/test/kotlin/be/alfu32/kotlin/starter/DefaultTest.kt
+++ src/test/kotlin/be/alfu32/kotlin/starter/DefaultTest.kt
@@ -1,11 +1,15 @@
 package be.alfu32.kotlin
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
 
 internal class DefaultTest {
 
   @Test
   public fun defaultTest(){
     println("hello")
+    assertEquals(1,1)
   }
 }
